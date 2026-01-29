f7a37c22b8d68ebde53bd24a5a373c5f0929048e
Emma Kealey <69314375+EmKealey@users.noreply.github.com>
2026-01-28 17:03:04 -0800

    update

--- MoTR/run_motr_in_magpie/emmaTest/src/magpie.config.js
+++ MoTR/run_motr_in_magpie/emmaTest/src/magpie.config.js
@@ -1,7 +1,8 @@
 export default {
   experimentId: '6',
+  serverUrl: 'https://cui-test-3.herokuapp.com/',
+  socketUrl: 'wss://cui-test-3.herokuapp.com/socket',
   // this will be used in prolific mode
-
   contactEmail: 'kealey@usc.edu',
   // Either 'debug', 'directLink' or 'prolific'
   mode: 'directLink',
